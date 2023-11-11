351ac66c98aff53c9f31b538680dcdf12ae88190
Harshita-Basera <basera.harshita.27@gmail.com>
2023-11-11 18:45:22 +0530

    initialise side bar and nav bar

--- src/App.js
+++ src/App.js
@@ -10,7 +10,19 @@ import { useState } from 'react';
 function App() {
 
   
-    
+  return (
+    <main>
+      <Navbar/>
+      <SideBar headColor={headColor} setHeadColor={setHeadColor} bg={bg} setBg={setBg} 
+      fromDate={fromDate} setFromDate={setFromDate} toDate={toDate} setToDate={setToDate} 
+      setFontColor={setFontColor} fontColor={fontColor} fontSize={fontSize} setFontSize={setFontSize} 
+      content={content} courseName={courseName} setCourseName={setCourseName} provider={provider} 
+      setProvider={setProvider}   setName={setName} name={name} borders={backgrounds} setBorder={setBorder} 
+       setContent={setContent} setForWhat={setForWhat}/>
+       
+      
+    </main>
+  );
 }
 
 export default App;
